5233e4d763b1021681f0ba92c8cf24b91a090bf3
HasnyByte <zzghif@gmail.com>
2025-04-23 10:40:35 +0700

    temp

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>test</title>
+</head>
+<body class="bg-black">
+<h1 class="font-bold hover:underline text-white">tes halaman</h1>
+</body>
+</html>
