4c317367b5fd6710619d2a17edd32e44190bb17e
HasnyByte <zzghif@gmail.com>
2025-04-23 11:03:30 +0700

    admin page without database

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/adminDashboard', function () {
+    return view('admin.dashboard');
+})->name('dashboard');
+
+Route::get('/adminPengguna', function () {
+    return view('admin.pengguna');
+})->name('pengguna');
+
+Route::get('/adminEvent', function () {
+    return view('admin.event');
+})->name('event');
+
+Route::get('/adminReview', function () {
+    return view('admin.review');
+})->name('review');
