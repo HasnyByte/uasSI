db9aa4481826fbb653651a584baa3fb765312fc1
HasnyByte <zzghif@gmail.com>
2025-05-04 02:02:46 +0700

    events page

--- resources/views/users/event.blade.php
+++ resources/views/users/event.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.users')
 
 @section('content')
-    <div>
-        <h1>Halaman Events</h1>
+    <div class="relative text-white bg-cover bg-center h-64" style="background-image: url('https://parksidehotels.co.id/wp-content/uploads/2025/02/494C204A-83C4-4504-920D-97F684A2D5FD.jpeg');">
+        <!-- Overlay gradient -->
+        <div class="absolute inset-0 bg-gradient-to-b from-black/50 via-black/30 to-black/20">
+            <div class="absolute inset-0 flex flex-col items-center justify-center z-10 text-center">
+                <div class="font-bold text-[30px]">
+                    What's on In Aceh
+                </div>
+                <div class="pt-2 text-[20px]">
+                    Looking for plans this week? Explore the latest art, music, shows, and festivals—start your adventure now!
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
